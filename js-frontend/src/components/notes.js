14d634a1f0e456056921c18124ab3adc222fc599
tiffnuugen <tiffanynuugen@gmail.com>
2021-03-22 18:42:09 -0400

    customize modules of and integrate quill with existing functionalities, create autosave feature, and use dompurify to sanitize html

--- js-frontend/src/components/notes.js
+++ js-frontend/src/components/notes.js
@@ -11,29 +11,44 @@ class Notes {
     this.noteList = document.querySelector('#note-list');
     this.newNoteBttn = document.querySelector('#new-note');
     this.noteContainer = document.querySelector('#note-container');
+    this.titleToolbar = [
+      [{ header: 1 }, { header: 2 }],
+      ['bold', 'italic', 'underline'],
+      ['clean']
+    ];
+    this.bodyToolbar = [
+      [{ header: [1, 2, 3, false] }],
+      ['bold', 'italic', 'underline'],
+      ['link', 'blockquote', 'code-block', 'image'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      ['clean']
+    ];
 
     this.noteList.addEventListener('click', (e) => {
       this.showNote(e);
       this.deleteNote(e);
     });
-    this.newNoteBttn.addEventListener('click', (e) => {
-      this.noteContainer.innerHTML = Note.renderNoteForm();
 
+    this.newNoteBttn.addEventListener('click', () => {
+      this.noteContainer.innerHTML = Note.renderNoteForm();
+      this.titleValidation = document.querySelector('#title-validation');
+      this.addNoteBttn = document.querySelector('#add-note');
       this.newTitle = new Quill(document.querySelector('#new-title'), {
         theme: 'snow',
         placeholder: "Don't forget the title...",
         modules: {
-          toolbar: [['bold', 'italic', 'underline'], [{ header: 1 }, { header: 2 }], ['clean']]
+          toolbar: this.titleToolbar
         }
       });
       this.newBody = new Quill(document.querySelector('#new-body'), {
         theme: 'snow',
-        placeholder: 'Start jotting here...'
+        placeholder: 'Start jotting here...',
+        modules: {
+          syntax: true,
+          toolbar: this.bodyToolbar
+        }
       });
-      this.titleValidation = document.querySelector('#title-validation');
-      this.addNoteBttn = document.querySelector('#add-note');
-
-      this.addNoteBttn.addEventListener('click', () => this.addNote(e.target.dataset.id));
+      this.addNoteBttn.addEventListener('click', () => this.addNote(this.newNoteBttn.dataset.id));
     });
   }
 
@@ -48,23 +63,24 @@ class Notes {
         <div class="message-body">
           <em>No notes found.</em>
         </div>
-      </article> `;
+      </article>`;
   }
 
   sortNotes(notes) {
     return notes.sort((a, b) => {
       const docA = new DOMParser().parseFromString(a.title, 'text/html');
       const docB = new DOMParser().parseFromString(b.title, 'text/html');
-      return docA.body.innerText.localeCompare(docB.body.innerText);
+      return docA.body.innerText.trim().localeCompare(docB.body.innerText.trim());
     });
   }
 
   fetchNotes(user) {
     if (!user) {
       this.adapter.getNotes().then((notes) => {
-        this.sortNotes(notes).forEach((note) => {
+        notes.forEach((note) => {
           if (!note.user) {
             this.notes.push(new Note(note));
+            this.sortNotes(this.notes);
           }
         });
         this.renderNoteList();
@@ -72,8 +88,9 @@ class Notes {
     } else {
       this.notes = [];
       if (user.notes.length > 0) {
-        this.sortNotes(user.notes).forEach((note) => {
+        user.notes.forEach((note) => {
           this.notes.push(new Note(note));
+          this.sortNotes(this.notes);
         });
       }
       this.name.innerText = user.name;
@@ -85,35 +102,26 @@ class Notes {
   showNote(e) {
     if (e.target.id === 'note-title') {
       this.adapter.getNote(e.target.dataset.id).then((note) => {
-        console.log('note shown', note);
         this.noteContainer.innerHTML = Note.renderNote(note);
-
-        this.saveNoteBttn = document.querySelector('#save-note');
-        this.savedMessage = document.querySelector('#saved-message');
-
         this.noteContainer.dataset.id = note.id;
-
+        this.saveStatus = document.querySelector('#save-status');
         this.title = new Quill(document.querySelector('#title'), {
           theme: 'bubble',
           placeholder: 'There should be a title here...',
           modules: {
-            toolbar: [['bold', 'italic', 'underline'], [{ header: 1 }, { header: 2 }], ['clean']]
+            toolbar: this.titleToolbar
           }
         });
         this.body = new Quill(document.querySelector('#body'), {
           theme: 'bubble',
           placeholder: "Jot here if you haven't already...",
           modules: {
-            toolbar: [
-              [{ header: [1, 2, 3, false] }],
-              ['bold', 'italic', 'underline', 'link'],
-              [{ list: 'ordered' }, { list: 'bullet' }],
-              ['clean']
-            ]
+            syntax: true,
+            toolbar: this.bodyToolbar
           }
         });
-
-        this.saveNoteBttn.addEventListener('click', () => this.editNote(note.id));
+        this.title.on('text-change', () => this.editNote(note.id));
+        this.body.on('text-change', () => this.editNote(note.id));
       });
     }
   }
@@ -136,27 +144,36 @@ class Notes {
     if (!this.newTitle.getText().trim().length) {
       this.handleTitleValidation();
     } else {
-      this.adapter
-        .postNote(this.newTitle.root.innerHTML, this.newBody.root.innerHTML, userId)
-        .then((note) => {
-          console.log('note added', note);
-          this.notes.push(new Note(note));
-          this.handleTitleValidation();
-          this.sortNotes(this.notes);
-          this.renderNoteList();
-        });
+      const newTitleEl = document.createElement('textarea');
+      const newBodyEl = document.createElement('textarea');
+      newTitleEl.innerHTML = this.newTitle.root.innerHTML;
+      newBodyEl.innerHTML = this.newBody.root.innerHTML;
+      const cleanNewTitle = DOMPurify.sanitize(newTitleEl.value);
+      const cleanNewBody = DOMPurify.sanitize(newBodyEl.value);
+      this.adapter.postNote(cleanNewTitle, cleanNewBody, userId).then((note) => {
+        this.notes.push(new Note(note));
+        this.handleTitleValidation();
+        this.sortNotes(this.notes);
+        this.renderNoteList();
+      });
     }
   }
 
   editNote(id) {
-    this.adapter.patchNote(this.title.innerText, this.body.value, id).then((editedNote) => {
+    clearTimeout(this.timerId);
+    this.saveStatus.innerHTML = '<em>(Saving...)</em>';
+    const titleEl = document.createElement('textarea');
+    const bodyEl = document.createElement('textarea');
+    titleEl.innerHTML = this.title.root.innerHTML;
+    bodyEl.innerHTML = this.body.root.innerHTML;
+    const cleanTitle = DOMPurify.sanitize(titleEl.value);
+    const cleanBody = DOMPurify.sanitize(bodyEl.value);
+    this.adapter.patchNote(cleanTitle, cleanBody, id).then((editedNote) => {
       this.notes = this.notes.map((note) => (note.id === id ? new Note(editedNote) : note));
-      this.savedMessage.classList.add('is-success');
-      this.savedMessage.innerText = 'Saved!';
-      setTimeout(() => (this.savedMessage.innerText = ''), 1000);
       this.sortNotes(this.notes);
       this.renderNoteList();
     });
+    this.timerId = setTimeout(() => (this.saveStatus.innerHTML = '<em>(Saved)</em>'), 2600);
   }
 
   deleteNote(e) {
