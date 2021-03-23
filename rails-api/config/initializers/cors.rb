e699a8101902823bbee551b4730852c8552560bc
tiffnuugen <tiffanynuugen@gmail.com>
2021-03-23 01:59:29 -0400

    change origin

--- rails-api/config/initializers/cors.rb
+++ rails-api/config/initializers/cors.rb
@@ -14,7 +14,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   # end
 
   allow do
-    origins 'https://jot.herokuapp.com'
+    origins 'https://jot-rails-api.herokuapp.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
