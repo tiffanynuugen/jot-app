4c07a8a8865968537cc701f197a74796a31b23b4
tiffnuugen <tiffanynuugen@gmail.com>
2021-03-23 02:30:39 -0400

    change base url to url of deployed backend

--- js-frontend/src/adapters/usersAdapter.js
+++ js-frontend/src/adapters/usersAdapter.js
@@ -1,6 +1,6 @@
 class UsersAdapter {
   constructor() {
-    this.baseUrl = 'http://localhost:3000/users';
+    this.baseUrl = 'https://jot-rails-api.herokuapp.com/users';
     this.headers = {
       Accept: 'application/json',
       'Content-Type': 'application/json'
